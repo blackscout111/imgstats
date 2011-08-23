3e6fdb9085f23cb7422196761597e54bcc75a44e
Bryan Clifford <blackscout111@gmail.com>
2011-08-22 23:52:39 -0700

    Takes an image file as an argument and calculates basic statistics of the image.

--- imgstats.cpp
+++ imgstats.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <cassert>
 
 #include <Magick++.h>
 
@@ -27,25 +28,56 @@ int main(int argc, char **argv)
 {
 	Magick::InitializeMagick(*argv);
 
-	// Construct the image object. Seperating image construction from the
+	// Construct the image object. Separating image construction from the
 	// the read operation ensures that a failure to read the image file
 	// doesn't render the image object useless.
-	Magick::Image image;
+	Magick::Image *image = new Magick::Image;
+	
+	// We're using 8 bit colors
+	assert(MaxRGB == 255);
 
-	matrix::matrix<unsigned char> mat(2,2);
-	unsigned char val = 0;
-	for (size_t i = 0; i < mat.height(); ++i)
+	try 
 	{
-		for (size_t j = 0; j < mat.width(); ++j)
-		{
-			mat(i,j) = ++val;
-		}
+		// Read a file into image object
+		image->read( argv[1] );
+	}
+	catch( Magick::Exception &error_ )
+	{
+		cout << "Caught exception: " << error_.what() << endl;
+		return EXIT_FAILURE;
 	}
 
-	printMat(mat);
-	printf("det(mat) = %f\n", matrix::det(mat));
-	printf("mean2d(mat) = %f\n", matrix::mean2d(mat));
-	printf("vari2d(mat) = %f\n", matrix::vari2d(mat,true));
+	// Image dimensions
+	size_t	nrows = image->rows(), ncols = image->columns();
+
+	matrix::matrix<unsigned char> rmat(nrows,ncols), gmat(nrows,ncols), bmat(nrows,ncols);
+	for (size_t row = 0; row < nrows; ++row)
+	{
+		for (size_t col = 0; col < ncols; ++col)
+		{
+			rmat(row,col) = (unsigned char)image->pixelColor(row, col).redQuantum();
+			gmat(row,col) = (unsigned char)image->pixelColor(row, col).greenQuantum();
+			bmat(row,col) = (unsigned char)image->pixelColor(row, col).blueQuantum();
+		}
+	}
+	delete image;
+
+	printf("image dimensions (nrows, ncols): %u %u\n", nrows, ncols);
+	printf("==== red layer ====\n");
+	printf("min = %u\n", matrix::min2d(rmat));
+	printf("max = %u\n", matrix::max2d(rmat));
+	printf("mean = %f\n", matrix::mean2d(rmat));
+	printf("stdev = %f\n", matrix::stdev2d(rmat,true));
+	printf("==== green layer ====\n");
+	printf("min = %u\n", matrix::min2d(gmat));
+	printf("max = %u\n", matrix::max2d(gmat));
+	printf("mean = %f\n", matrix::mean2d(gmat));
+	printf("stdev = %f\n", matrix::stdev2d(gmat,true));
+	printf("==== blue layer ====\n");
+	printf("min = %u\n", matrix::min2d(bmat));
+	printf("max = %u\n", matrix::max2d(bmat));
+	printf("mean = %f\n", matrix::mean2d(bmat));
+	printf("stdev = %f\n", matrix::stdev2d(bmat,true));
 
 	return EXIT_SUCCESS;
 }
