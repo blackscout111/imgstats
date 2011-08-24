c0ce9ca07e6a3550b45e900d53db806133ca710a
Bryan Clifford <blackscout111@gmail.com>
2011-08-24 16:47:37 -0700

    Added better formating and stats to histograms

--- imgstats.cpp
+++ imgstats.cpp
@@ -1,3 +1,11 @@
+/* Filename: imgstats.cpp
+ * Description: This is a simple program that calculates image layer statistics.
+ * Input Parameters: [Image File Name]
+ * Author: Bryan A. Clifford
+ * Last Updated: 08.24.2011
+ */
+
+
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
@@ -32,7 +40,26 @@ void initROOT(const char* appClassName)
 	// Set the canvas style used
 	gROOT->SetStyle("Plain");		// Start using the plain style
 	gStyle->SetPalette(1);			// Get access to a fancy color palette
-	gStyle->SetOptStat(1);			// Show/Remove the stats box
+
+	// Display Histogram Statistics Box
+	// The type of information printed in the histogram statistics box
+	//  can be selected via the parameter mode.
+	//
+	//	Example: gStyle->SetOptStat("ne");  Display name and number of entries
+	//    k  kurtosis printed
+	//    K  kurtosis and kurtosis error printed
+	//    s  skewness printed
+	//    S  skewness and skewness error printed
+	//    i  integral of bins printed
+	//    o  number of overflows printed
+	//    u  number of underflows printed
+	//    r  rms printed
+	//    R  rms and rms error printed
+	//    m  mean value printed
+	//    M  mean and mean error values printed
+	//    e  number of entries printed
+	//    n  name of histogram is printed
+	gStyle->SetOptStat("nemrks");
 
 	// Set some title properties
 	gStyle->SetTitleW(0.5);
@@ -49,6 +76,8 @@ void initROOT(const char* appClassName)
 //_____________________________________________________________________________
 void dispImgStats(const Magick::Image* image)
 {
+	// No NULLs
+	assert(image);
 
 	// Initialize ROOT
 	initROOT("imgstats");
@@ -59,18 +88,18 @@ void dispImgStats(const Magick::Image* image)
 			*bhist = NULL;
 
 	// Canvases
-	TCanvas *rcan = new TCanvas("rcan");
-	TCanvas *gcan = new TCanvas("gcan");
-	TCanvas *bcan = new TCanvas("bcan");
+	TCanvas *rcan = new TCanvas("rcan", "imgstats - Red Layer Statistics");
+	TCanvas *gcan = new TCanvas("gcan", "imgstats - Green Layer Statistics");
+	TCanvas *bcan = new TCanvas("bcan", "imgstats - Blue Layer Statistics");
 
     // Make the histograms
-    rhist = new TH1F("rhist", "Red Layer", 256, 0, 255);
+    rhist = new TH1F("Red Layer", "Red Layer Statistics", 256, -0.5, 255.5);
     rhist->GetXaxis()->SetTitle("Pixel Intensity");
 	rhist->GetYaxis()->SetTitle("# of Pixels");
-    ghist = new TH1F("ghist", "Green Layer", 256, 0, 255);
+    ghist = new TH1F("Green Layer", "Green Layer Statistics", 256, -0.5, 255.5);
     ghist->GetXaxis()->SetTitle("Pixel Intensity");
 	ghist->GetYaxis()->SetTitle("# of Pixels");
-    bhist = new TH1F("bhist", "Blue Layer", 256, 0, 255);
+    bhist = new TH1F("Blue Layer", "Blue Layer Statistics", 256, -0.5, 255.5);
     bhist->GetXaxis()->SetTitle("Pixel Intensity");
 	bhist->GetYaxis()->SetTitle("# of Pixels");
 
@@ -94,18 +123,26 @@ void dispImgStats(const Magick::Image* image)
 
     // Draw the histograms
 	rhist->SetLineColor(kRed);
+	rhist->SetFillColor(kRed);
 	ghist->SetLineColor(kGreen);
+	ghist->SetFillColor(kGreen);
 	bhist->SetLineColor(kBlue);
+	bhist->SetFillColor(kBlue);
 	rcan->cd();
-    rhist->Draw("");
+	rhist->Draw();
 	gcan->cd();
-    ghist->Draw("");
+	ghist->Draw();
 	bcan->cd();
-    bhist->Draw("");
+	bhist->Draw();
 
 	// Program will pause at this next line until you go File->Exit from the X11 screen
-	printf("In the X11 window...\n go to File-->\"Close Canvas\" to close\n");
+	printf("In an X11 window go to File-->\"Close Canvas\" to close\n");
 	gApplication->Run(kTRUE);
+
+	// Delete the canvases
+	delete rcan;
+	delete gcan;
+	delete bcan;
 
     // Delete the histograms
     delete rhist;
@@ -121,7 +158,7 @@ int main(int argc, char **argv)
 	// Construct the image object. Separating image construction from the
 	// the read operation ensures that a failure to read the image file
 	// doesn't render the image object useless.
-	Magick::Image *image = new Magick::Image;
+	Magick::Image *image = NULL;
 	
 	// We're using 8 bit colors
 	assert(MaxRGB == 255);
@@ -130,9 +167,18 @@ int main(int argc, char **argv)
 	try 
 	{
 		// Read a file into image object
-		image->read( argv[1] );
+		if (argc == 2)
+		{
+			image = new Magick::Image;
+			image->read(argv[1]);
+		}
+		else
+		{
+			printf("Incorrect number of input parameters.\n");
+			printf("Usage:\n\n\t./imgstats [FILE NAME]\n");
+		}
 	}
-	catch( Magick::Exception &error_ )
+	catch(Magick::Exception &error_)
 	{
 		cout << "Caught exception: " << error_.what() << endl;
 		return EXIT_FAILURE;
@@ -140,12 +186,15 @@ int main(int argc, char **argv)
 	printf("done\n");
 	
 	// Display the histograms
-	dispImgStats(image);
+	if (image)
+	{
+		dispImgStats(image);
 
-	// Free the image from memory
-	printf("freeing image memory... ");
-	delete image;
-	printf("done\n");
+		// Free the image from memory
+		printf("freeing image memory... ");
+		delete image;
+		printf("done\n");
+	}
 
 	return EXIT_SUCCESS;
 }
